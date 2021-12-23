535d66d12a8a57336ff412fe3f5838a447067bb7
Stefan <hardmaster.92@hotmail.com>
2021-12-23 20:55:46 +0200

    bump some major versions. remove lodash.

--- src/populateDatabase.ts
+++ src/populateDatabase.ts
@@ -1,29 +1,28 @@
 import { hash } from 'bcrypt';
 import faker from 'faker';
-import { random, times } from 'lodash';
 import { db, intializedSequelize } from './sequelize';
 
 export default (): Promise<void> => intializedSequelize
-// Clean tables
+  // Clean tables
   .sync({ force: true })
   .then(async () => {
     console.log('Populate database wit fake data');
-    await db.User.bulkCreate(
-      times(10, () => ({
+
+    await db.User.bulkCreate(await Promise.all(
+      Array.from({ length: 10 }).map(async () => ({
         email: faker.internet.email(),
         fullName: `${faker.name.firstName()} ${faker.name.lastName()}`,
-        password: hash(faker.internet.password(), 8),
-        role: random(1, 5),
+        password: await hash(faker.internet.password(), 8),
+        role: faker.datatype.number({ min: 1, max: 5 }),
         username: faker.internet.userName(),
       })),
-    );
-    await db.BlogPost.bulkCreate(
-      times(10, () => ({
-        description: faker.lorem.sentence(20),
-        published: random(1, 2) === 1,
-        title: faker.lorem.sentence(5),
-        userId: random(1, 10),
-      })),
-    );
+    ));
+
+    await db.BlogPost.bulkCreate(Array.from({ length: 10 }).map(() => ({
+      description: faker.lorem.sentence(20),
+      published: faker.datatype.number({ min: 1, max: 2 }) === 1,
+      title: faker.lorem.sentence(5),
+      userId: faker.datatype.number({ min: 1, max: 10 }),
+    })));
     console.log('Database populated');
   });
